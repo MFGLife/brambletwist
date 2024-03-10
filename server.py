523936397b1e649234aef4ccaffb7e6836fae906
Micheal Salmon <micheal.mfg@gmail.com>
2024-03-10 10:01:36 -0500

    added donate

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socketserver
 import os
 
 # Set the port number
-port = 8007
+port = 8020
 
 # Set the directory to the current working directory
 directory = '.'  # Uncomment this line if you want test.html to be served for any path
